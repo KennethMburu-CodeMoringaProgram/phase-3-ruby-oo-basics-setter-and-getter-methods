5c16c33ce4036a8dbae33ec48564b820853822b6
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-02-08 17:52:22 +0300

    used getter to dog class and person class

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,18 @@
 class Dog
+    attr_accessor :name, :breed
+  
+    def initialize(name, breed)
+      @name = name
+      @breed = breed
+    end
+  end
+  
 
-end
+first_dog = Dog.new("Rufus", "Golden Retriever")
+second_dog = Dog.new("Lex", "Golden Dark")
+
+puts first_dog.name # Output: Rufus
+puts first_dog.breed # Output: Golden Retriever
+
+puts second_dog.name # Output: Rufus
+puts second_dog.breed # Output: Golden Retriever
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,13 @@
 class Person
-
+    attr_accessor :name, :job
+  
+    def initialize(name, job)
+      @name = name
+      @job = job
+    end
 end
+
+person = Person.new("Dean, Wnchester", "Full stck Developer")
+
+puts person.name
+puts person.job 
